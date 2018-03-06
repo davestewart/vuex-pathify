fd5d98ba7ab9acf29b10117348d206fad5cf0f6e
Dave Stewart <dave@davestewart.co.uk>
2018-03-06 10:28:15 +0000

    Implemented per-module persistance

--- src/plugins/persist.js
+++ src/plugins/persist.js
@@ -2,17 +2,51 @@ const storage = window.localStorage
 
 import { merge } from '../utils/object'
 
-const defaults = {
+export const defaults = {
   key: 'vuex'
+}
+
+function getData (state, save) {
+  // ask module to filter state
+  if (save instanceof Function) {
+    return save(state)
+  }
+
+  // save only keys of state
+  else if (Array.isArray(save)) {
+    return save
+      .reduce((output, key) => {
+        output[key] = state[key]
+        return output
+      }, {})
+  }
+
+  // save entire state
+  else if (save === true) {
+    return state
+  }
 }
 
 /**
  * Storage helper to load, save and remove state from local storage
  *
  * @param   {Vuex}    store
- * @param   {object}  options
  */
-export default function (store, { key } = defaults) {
+export default function (store) {
+
+  const { key } = defaults
+
+  // module callbacks
+  const children = store._modules.root._children
+  const callbacks = Object
+    .keys(children)
+    .reduce((callbacks, key) => {
+      callbacks[key] = children[key]._rawModule.save
+      return callbacks
+    }, {})
+
+  // root callback
+  callbacks._root = store._modules.root._rawModule.save
 
   /**
    * Save data to local storage
@@ -21,9 +55,38 @@ export default function (store, { key } = defaults) {
    * @param {Object}                    state       The store state
    */
   function save (mutation, state) {
-    // TODO implement save function
-    // TODO look at filters
-    storage.setItem(key, JSON.stringify(state))
+    // variables
+    const output = {}
+    const root = {}
+
+    // loop over state
+    Object
+      .keys(state)
+      .reduce((output, name) => {
+        // modules
+        if (name in callbacks) {
+          const data = getData(state[name], callbacks[name])
+          if (data) {
+            output[name] = data
+          }
+        }
+
+        // root
+        else {
+          root[name] = state[name]
+        }
+
+        // return
+        return output
+      }, output)
+
+    // collate root
+    const data = getData(root, callbacks._root)
+    if (data) {
+      Object.assign(output, data)
+    }
+
+    storage.setItem(key, JSON.stringify(output))
   }
 
   /**
