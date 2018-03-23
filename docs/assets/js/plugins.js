270f2b4a684bf47bfa66674754caa69c905cdd3b
Dave Stewart <dave@davestewart.co.uk>
2018-03-23 01:53:47 +0000

    Added anchor-fix plugin

---
+++ docs/assets/js/plugins.js
@@ -0,0 +1,61 @@
+function footer (hook) {
+  var footer = [
+    '<hr/>',
+    '<footer>',
+    '<span><a href="https://github.com/QingWei-Li">cinwell</a> &copy;2017.</span>',
+    '<span>Proudly published with <a href="https://github.com/QingWei-Li/docsify" target="_blank">docsify</a>.</span>',
+    '</footer>'
+  ].join('')
+
+  hook.afterEach(function (html) {
+    return html + footer
+  })
+}
+
+/**
+ * Fix anchors for all headings with code in them
+ *
+ * @param hook
+ */
+function fixAnchors (hook) {
+
+  hook.afterEach(function (html, next) {
+
+    // find all headings and replace them
+    html = html.replace(/<(h\d).+?<\/\1>/g, function (html) {
+
+      // create temp node
+      var div = document.createElement('div')
+      div.innerHTML = html
+
+      // get anchor
+      var link = div.querySelector('a[href*="?id"]')
+
+      if (!link) {
+        return html
+      }
+
+      // work out id
+      var text = link.innerText
+      var id = text
+        .split('(')
+        .shift()
+        .toLowerCase()
+        .replace(/\W+/g, '-')
+        .replace(/^-+|-+$/g, '')
+      var href = link.getAttribute('href')
+        .replace(/\?id=.+/, '?id=' + id)
+
+      // update dom
+      link.setAttribute('href', href)
+      link.setAttribute('data-id', id)
+      link.parentElement.setAttribute('id', id)
+
+      // return html
+      return div.innerHTML
+    })
+
+    // continue
+    next(html)
+  })
+}
