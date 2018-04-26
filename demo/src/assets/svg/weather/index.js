c0cb454cb7dc341727ad562c5227fcda7158d4b5
Dave Stewart <dave@davestewart.co.uk>
2018-04-26 20:54:22 +0100

    Unfixed .svg.txt temp fix

--- demo/src/assets/svg/weather/index.js
+++ demo/src/assets/svg/weather/index.js
@@ -1,13 +1,12 @@
-// .txt is temp fix to force codesandbox to load SVGs as text
 export default {
-  'snow': require('!!raw-loader!./snow.svg.txt'),
-  'wind': require('!!raw-loader!./wind.svg.txt'),
-  'leaf': require('!!raw-loader!./leaf.svg.txt'),
-  'hail': require('!!raw-loader!./hail.svg.txt'),
-  'storm': require('!!raw-loader!./storm.svg.txt'),
-  'cloudy': require('!!raw-loader!./cloudy.svg.txt'),
-  'autumn': require('!!raw-loader!./autumn.svg.txt'),
-  'rainbow': require('!!raw-loader!./rainbow.svg.txt'),
-  'tornado': require('!!raw-loader!./tornado.svg.txt'),
-  'sunrise': require('!!raw-loader!./sunrise.svg.txt'),
+  'snow': require('!raw-loader!./snow.svg'),
+  'wind': require('!raw-loader!./wind.svg'),
+  'leaf': require('!raw-loader!./leaf.svg'),
+  'hail': require('!raw-loader!./hail.svg'),
+  'storm': require('!raw-loader!./storm.svg'),
+  'cloudy': require('!raw-loader!./cloudy.svg'),
+  'autumn': require('!raw-loader!./autumn.svg'),
+  'rainbow': require('!raw-loader!./rainbow.svg'),
+  'tornado': require('!raw-loader!./tornado.svg'),
+  'sunrise': require('!raw-loader!./sunrise.svg'),
 }
