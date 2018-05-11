c76bc7f9bd6c1b5e53bbb528b616ec35ee454db6
Dave Stewart <dave@davestewart.co.uk>
2018-05-11 13:25:16 +0100

    Moved demo to new repository: https://github.com/davestewart/vuex-pathify-demos

--- demo/src/app/router/simple.js
+++
@@ -1,7 +0,0 @@
-import { route } from './index'
-
-import Hello from '../../examples/simple/Hello'
-
-export default [
-  route('/simple/hello', Hello),
-]
--- demo/src/app/store/pathify.js
+++
@@ -1,5 +0,0 @@
-import pathify from 'vuex-pathify'
-
-pathify.options.mapping = 'standard'
-
-export default pathify
